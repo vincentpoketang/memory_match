92f99d75d12841dd54866cbec9e92cae58aa19a0
Vincent Tang <vincet3@uci.edu>
2017-04-20 16:19:15 -0700

    version1.0 - Vincent Tang (added randomization)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 9;
 var match_counter = 0;
+var is_timeout_done = true;
 //Stats
 var attempts = 0;
 var accuracy = 0;
@@ -22,45 +23,68 @@ function reset_stats(){
 
 function card_clicked(element){
     //show card face
+    $(element).find('.front').before($(element).find('.back'));
     $(element).find('.front').removeClass('transparent');
-    if(first_card_clicked===null){
+    if(first_card_clicked===null){ // Add to first_card_clicked if nothing inside
         first_card_clicked = $(element);
     }
-    else{
+    else{ // Add to seond_card_clicked
         second_card_clicked = $(element);
         attempts++;
-        if($(first_card_clicked).find('.front').find('img').attr('src') === $(second_card_clicked).find('.front').find('img').attr('src')){
+        if($(first_card_clicked).find('.front').find('img').attr('src') === $(second_card_clicked).find('.front').find('img').attr('src')){ // compare front of two card together and if true match stuff
             match_counter++;
             accuracy = match_counter/attempts;
             first_card_clicked = null;
             second_card_clicked = null;
             display_stats();
-            if(match_counter === total_possible_matches){
+            if(match_counter === total_possible_matches){ // check for overall win
                 setTimeout(function(){
                     alert("You win!");
                 }, 500);
             }
         }
-        else{
+        else{ // Cards does not match, reset stuff
             accuracy = match_counter/attempts;
             display_stats();
+            is_timeout_done = false;
             setTimeout(function(){
+                $(first_card_clicked).find('.back').before($(first_card_clicked).find('.front'));
+                $(second_card_clicked).find('.back').before($(second_card_clicked).find('.front'));
                 $(first_card_clicked).find('.front').addClass("transparent");
                 $(second_card_clicked).find('.front').addClass("transparent");
                 first_card_clicked = null;
                 second_card_clicked = null;
-            }, 1000);
+                is_timeout_done = true;
+            }, 2000);
         }
+    }
+}
+function randomize_cards(){
+    var cards = $('.card');
+    for(var i = 0; i<cards.length; i++){
+        var target = Math.floor(Math.random() * cards.length -1) + 1;
+        var target2 = Math.floor(Math.random() * cards.length -1) + 1;
+        cards[target].before(cards[target2]);
     }
 }
 $(document).ready(function(){
     display_stats();
+    randomize_cards();
     $('.card').click(function(){
-        card_clicked(this);
+        if($(this).find('.front').hasClass('transparent') && is_timeout_done){
+            card_clicked(this);
+        }
     });
     $('.reset').click(function(){
-        games_played++;
-        reset_stats();
-        $('.front').addClass('transparent');
+        if(attempts!==0){
+            games_played++;
+            reset_stats();
+            var cards = $('.card');
+            for(var i=0;i<cards.length;i++){
+                $(cards[i]).find('.back').before($(cards[i]).find('.front'));
+            }
+            $('.front').addClass('transparent');
+            randomize_cards();
+        }
     })
 });
