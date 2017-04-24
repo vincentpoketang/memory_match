cbb68b33c0ebcc2693d20f51408976d3497a6d33
Vincent Tang <vincet3@uci.edu>
2017-04-24 11:30:12 -0700

    added evolution when matched

--- script.js
+++ script.js
@@ -1,13 +1,91 @@
 //Functionality
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 9;
+var total_possible_matches = 18;
 var match_counter = 0;
 var is_timeout_done = true;
 //Stats
 var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
+var pokemon_evolution_chart = {
+    'images/bulbasaur.gif':'images/ivysaur.gif',
+    'images/ivysaur.gif':'images/venusaur.gif',
+    'images/charmander.gif':'images/charmeleon.gif',
+    'images/charmeleon.gif':'images/charizard.gif',
+    'images/squirtle.gif':'images/wartortle.gif',
+    'images/wartortle.gif':'images/blastoise.gif',
+    'images/chikorita.gif':'images/bayleef.gif',
+    'images/bayleef.gif':'images/meganium.gif',
+    'images/cyndaquil.gif':'images/quilava.gif',
+    'images/quilava.gif':'images/typhlosion.gif',
+    'images/totodile.gif':'images/croconaw.gif',
+    'images/croconaw.gif':'images/feraligatr.gif',
+    'images/treecko.gif':'images/grovyle.gif',
+    'images/grovyle.gif':'images/sceptile.gif',
+    'images/torchic.gif':'images/combusken.gif',
+    'images/combusken.gif':'images/blaziken.gif',
+    'images/mudkip.gif':'images/marshtomp.gif',
+    'images/marshtomp.gif':'images/swampert.gif'
+};
+var pokemon_first_state = {
+    'images/venusaur.gif':'images/bulbasaur.gif',
+    'images/charizard.gif':'images/charmander.gif',
+    'images/blastoise.gif':'images/squirtle.gif',
+    'images/meganium.gif':'images/chikorita.gif',
+    'images/typhlosion.gif':'images/cyndaquil.gif',
+    'images/feraligatr.gif':'images/totodile.gif',
+    'images/sceptile.gif':'images/treecko.gif',
+    'images/blaziken.gif':'images/torchic.gif',
+    'images/swampert.gif':'images/mudkip.gif',
+    'images/ivysaur.gif':'images/bulbasaur.gif',
+    'images/charmeleon.gif':'images/charmander.gif',
+    'images/wartortle.gif':'images/squirtle.gif',
+    'images/bayleef.gif':'images/chikorita.gif',
+    'images/quilava.gif':'images/cyndaquil.gif',
+    'images/croconaw.gif':'images/totodile.gif',
+    'images/grovyle.gif':'images/treecko.gif',
+    'images/combusken.gif':'images/torchic.gif',
+    'images/marshtomp.gif':'images/mudkip.gif',
+    'images/bulbasaur.gif':'images/bulbasaur.gif',
+    'images/charmander.gif':'images/charmander.gif',
+    'images/squirtle.gif':'images/squirtle.gif',
+    'images/chikorita.gif':'images/chikorita.gif',
+    'images/cyndaquil.gif':'images/cyndaquil.gif',
+    'images/totodile.gif':'images/totodile.gif',
+    'images/treecko.gif':'images/treecko.gif',
+    'images/torchic.gif':'images/torchic.gif',
+    'images/mudkip.gif':'images/mudkip.gif'
+}
+var pokemon_list = {
+    'images/bulbasaur.gif':['Bulbasaur','height_50px'],
+    'images/ivysaur.gif':['Ivysaur','height_70px'],
+    'images/venusaur.gif':['Venusaur','height_90px'],
+    'images/charmander.gif':['Charmander','height_60px'],
+    'images/charmeleon.gif':['Charmeleon','height_70px'],
+    'images/charizard.gif':['Charizard','height_130px'],
+    'images/squirtle.gif':['Squirtle','height_50px'],
+    'images/wartortle.gif':['Wartortle','height_70px'],
+    'images/blastoise.gif':['Blastoise','height_90px'],
+    'images/chikorita.gif':['Chikorita','height_60px'],
+    'images/bayleef.gif':['Bayleef','height_90px'],
+    'images/meganium.gif':['Meganium','height_90px'],
+    'images/cyndaquil.gif':['Cyndaquil','height_40px'],
+    'images/quilava.gif':['Quilava','height_40px'],
+    'images/typhlosion.gif':['Typhlosion','height_90px'],
+    'images/totodile.gif':['Totodile','height_60px'],
+    'images/croconaw.gif':['Croconaw','height_70px'],
+    'images/feraligatr.gif':['Feraligatr','height_90px'],
+    'images/treecko.gif':['Treecko','height_60px'],
+    'images/grovyle.gif':['Grovyle','height_70px'],
+    'images/sceptile.gif':['Sceptile','height_90px'],
+    'images/torchic.gif':['Torchic','height_60px'],
+    'images/combusken.gif':['Combusken','height_80px'],
+    'images/blaziken.gif':['Blaziken','height_90px'],
+    'images/mudkip.gif':['Mudkip','height_60px'],
+    'images/marshtomp.gif':['Marshtomp','height_80px'],
+    'images/swampert.gif':['Swampert','height_90px']
+};
 function display_stats(){
     $('.games_played .value').text(games_played);
     $('.matches .value').text(match_counter);
@@ -28,22 +106,22 @@ function card_clicked(element){
     if(first_card_clicked===null){ // Add to first_card_clicked if nothing inside
         first_card_clicked = $(element);
     }
-    else{ // Add to seond_card_clicked
+    else{ // Add to second_card_clicked
         second_card_clicked = $(element);
         attempts++;
         if($(first_card_clicked).find('.front').find('img').attr('src') === $(second_card_clicked).find('.front').find('img').attr('src')){ // compare front of two card together and if true match stuff
             match_counter++;
             accuracy = match_counter/attempts;
+            switch_img_of_matched_pokemon(first_card_clicked,second_card_clicked);
             first_card_clicked = null;
             second_card_clicked = null;
             display_stats();
             if(match_counter === total_possible_matches){ // check for overall win
-                setTimeout(function(){
-                    alert("You win!");
-                }, 500);
+                $('#info-box').text("You win!");
             }
         }
         else{ // Cards does not match, reset stuff
+            $('#info-box').text("That's not the same Pokemon!");
             accuracy = match_counter/attempts;
             display_stats();
             is_timeout_done = false;
@@ -54,6 +132,7 @@ function card_clicked(element){
                 $(second_card_clicked).find('.front').addClass("transparent");
                 first_card_clicked = null;
                 second_card_clicked = null;
+                $('#info-box').append("<br>They both ran back into their grass!");
                 is_timeout_done = true;
             }, 2000);
         }
@@ -120,25 +199,50 @@ function grass_move_when_hover(){
         isAnimated = false;
     })
 }
+function switch_img_of_matched_pokemon(first_card,second_card){
+    var card_img = $(first_card).find('.front').find('img').attr('src');
+    var pre_evolution_card_img = card_img;
+    // Change first and second card image to evolved state
+    $(first_card).find('.front').find('img').attr('src', pokemon_evolution_chart[card_img]);
+    $(second_card).find('.front').find('img').attr('src', pokemon_evolution_chart[card_img]);
+    // // Remove height for pre-evolution
+    $(first_card).find('.front').removeClass(pokemon_list[card_img][1]);
+    $(second_card).find('.front').removeClass(pokemon_list[card_img][1]);
+    // Add new height for evolution
+    $(first_card).find('.front').addClass(pokemon_list[pokemon_evolution_chart[card_img]][1]);
+    $(second_card).find('.front').addClass(pokemon_list[pokemon_evolution_chart[card_img]][1]);
+    card_img = $(first_card).find('.front').find('img').attr('src');
+    $('#info-box').text(pokemon_list[pre_evolution_card_img][0] +" evolved into " + pokemon_list[card_img][0] + "!");
+    if(pokemon_evolution_chart[card_img] !== undefined) {
+        is_timeout_done = false;
+        setTimeout(function(){
+            $(first_card).find('.front').addClass('transparent');
+            $(second_card).find('.front').addClass('transparent');
+            randomize_cards();
+            $('#info-box').append("<br>Every Pokemon ran into a different grass!")
+            is_timeout_done = true;
+        },2000);
+    }
+}
 $(document).ready(function(){
     creating_div_block('front height_50px transparent','images/bulbasaur.gif');
     creating_div_block('front height_50px transparent','images/bulbasaur.gif');
-    creating_div_block('front transparent','images/charmander.gif');
-    creating_div_block('front transparent','images/charmander.gif');
+    creating_div_block('front height_60px transparent','images/charmander.gif');
+    creating_div_block('front height_60px transparent','images/charmander.gif');
     creating_div_block('front height_50px transparent','images/squirtle.gif');
     creating_div_block('front height_50px transparent','images/squirtle.gif');
-    creating_div_block('front transparent','images/chikorita.gif');
-    creating_div_block('front transparent','images/chikorita.gif');
+    creating_div_block('front height_60px transparent','images/chikorita.gif');
+    creating_div_block('front height_60px transparent','images/chikorita.gif');
     creating_div_block('front height_40px transparent','images/cyndaquil.gif');
     creating_div_block('front height_40px transparent','images/cyndaquil.gif');
-    creating_div_block('front transparent','images/totodile.gif');
-    creating_div_block('front transparent','images/totodile.gif');
-    creating_div_block('front transparent','images/treecko.gif');
-    creating_div_block('front transparent','images/treecko.gif');
-    creating_div_block('front transparent','images/torchic.gif');
-    creating_div_block('front transparent','images/torchic.gif');
-    creating_div_block('front transparent','images/mudkip.gif');
-    creating_div_block('front transparent','images/mudkip.gif');
+    creating_div_block('front height_60px transparent','images/totodile.gif');
+    creating_div_block('front height_60px transparent','images/totodile.gif');
+    creating_div_block('front height_60px transparent','images/treecko.gif');
+    creating_div_block('front height_60px transparent','images/treecko.gif');
+    creating_div_block('front height_60px transparent','images/torchic.gif');
+    creating_div_block('front height_60px transparent','images/torchic.gif');
+    creating_div_block('front height_60px transparent','images/mudkip.gif');
+    creating_div_block('front height_60px transparent','images/mudkip.gif');
     grass_move_when_hover();
     display_stats();
     randomize_cards();
@@ -154,8 +258,12 @@ $(document).ready(function(){
             reset_stats();
             var cards = $('.card');
             for(var i=0;i<cards.length;i++){
-                $(cards[i]).find('.back').before($(cards[i]).find('.front'));
+                $(cards[i]).find('.back').before($(cards[i]).find('.front')); // make so grass is in front of pokemon
+                $(cards[i]).find('.front').removeClass(pokemon_list[$(cards[i]).find('.front').find('img').attr('src')][1]);
+                $(cards[i]).find('.front').find('img').attr('src',pokemon_first_state[$(cards[i]).find('.front').find('img').attr('src')]);
+                $(cards[i]).find('.front').addClass(pokemon_list[pokemon_first_state[$(cards[i]).find('.front').find('img').attr('src')]][1]);
             }
+            $('#info-box').text("Welcome to Pokemon Memory Match! Find a matched set of Pokemon and try to catch them all!");
             $('.front').addClass('transparent');
             randomize_cards();
         }
